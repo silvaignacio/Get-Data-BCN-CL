883c873baecf58a2cbef5bbe0769306ba295c377
Ignacio Silva Silva <iandsilvas@falabella.cl>
2022-07-09 19:30:59 -0400

    feat: add sast checker

--- get-data-bcn.go
+++ get-data-bcn.go
@@ -6,49 +6,50 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-type Organismos struct {
-	XMLName   xml.Name `xml:"ORGANISMOS"`
-	Organismo string   `xml:"ORGANISMO"`
+type Organisms struct {
+	XMLName  xml.Name `xml:"ORGANISMOS"`
+	Organism string   `xml:"ORGANISMO"`
 }
 
-type Norma struct {
-	XMLName           xml.Name   `xml:"NORMA"`
-	FechaPublicacion  string     `xml:"FECHA_PUBLICACION"`
-	FechaPromulgacion string     `xml:"FECHA_PROMULGACION"`
-	Titulo            string     `xml:"TITULO"`
-	Texto             string     `xml:"TEXTO"`
-	Organismos        Organismos `xml:"ORGANISMOS"`
+type Rule struct {
+	XMLName         xml.Name  `xml:"NORMA"`
+	PublishedDate   string    `xml:"FECHA_PUBLICACION"`
+	PromulgatedDate string    `xml:"FECHA_PROMULGACION"`
+	Title           string    `xml:"TITULO"`
+	Text            string    `xml:"TEXTO"`
+	Organisms       Organisms `xml:"ORGANISMOS"`
 }
 
-type NormasResult struct {
+type RulesResult struct {
 	XMLName xml.Name `xml:"NORMAS"`
-	Norma   []Norma  `xml:"NORMA"`
+	Rule    []Rule   `xml:"NORMA"`
 }
 
-type NormaChilenaResponse struct {
-	NormasResult string `xml:"NormasResult"`
+type ChileanRulesResponse struct {
+	RulesResult string `xml:"RulesResult"`
 }
 
 var (
-	r NormaChilenaResponse
+	r ChileanRulesResponse
 )
 
 func getDataBCN(c *gin.Context) {
-	resp, _ := http.Get("https://www.leychile.cl/Consulta/obtxml?opt=3&cantidad=5")
+	resp, _ := http.Get(os.Getenv("BCN_URL"))
 
 	defer resp.Body.Close()
 
 	data, _ := ioutil.ReadAll(resp.Body)
-	result := NormasResult{}
+	result := RulesResult{}
 
 	_ = xml.Unmarshal([]byte(data), &result)
 
-	for i := 0; i < len(result.Norma); i++ {
-		fmt.Println("Título: " + result.Norma[i].Titulo)
-		fmt.Println("Fecha de promulgación: " + result.Norma[i].FechaPromulgacion)
-		fmt.Println("Organismo: " + result.Norma[i].Organismos.Organismo)
+	for i := 0; i < len(result.Rule); i++ {
+		fmt.Println("Título: " + result.Rule[i].Title)
+		fmt.Println("Fecha de promulgación: " + result.Rule[i].PromulgatedDate)
+		fmt.Println("Organismo: " + result.Rule[i].Organisms.Organism)
 		fmt.Println(" ")
 	}
 
